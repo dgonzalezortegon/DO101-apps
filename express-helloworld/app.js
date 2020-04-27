a364477b3265b627f48e37b0ef059cc40584c146
Daniel González Ortegón <dgonzalezo@gmail.com>
2020-04-27 17:20:30 +0200

    hello mars click in the same page

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,12 +2,12 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
-});
+  res.send('<html><b>Hello World   my name is Dani!</b> <br> <body>  <br> <h2> Click here ->  <a href=mars> here </a> to go to mars.</h2></body></html>')
+  });
 
 
 app.get('/mars', function (req, res) {
-  res.send('Hello Mars!\n');
+  res.send('<h1 >Dani says  Hello to Mars!</h1>\n');
 });
 
 
